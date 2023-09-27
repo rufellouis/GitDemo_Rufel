d22af35d8980ee8e93eb1e781c052f34eee5c21c
Rufel Estrada <31424076+rufellouis@users.noreply.github.com>
2023-09-26 22:39:38 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+
+def read_csv_into_sparkdf():
+
+    # Create a Spark session
+    spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
+
+    # Define the file path
+    file_path = "example-data.csv"
+
+    # Read the CSV file into a DataFrame
+    df = spark.read.csv(file_path, header=True, inferSchema=True)
+
+    # Show the DataFrame
+    df.show()
+
+    # Output the spark dataframe as df
+    return df
+
+if __name__ == "__main__":
+    df = read_csv_into_sparkdf()
